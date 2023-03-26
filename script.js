eec26273afedf519b915314cc6ede8f43fc53157
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-03-26 23:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+  // Remove all non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.toLowerCase().replace(/[^0-9a-z]/gi, '');
+  
+  // Check if clean string is equal to its reverse
+  return cleanStr === cleanStr.split('').reverse().join('');
 }
-module.exports = palindrome
+
+module.exports = palindrome;
